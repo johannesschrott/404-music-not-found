c33510ffc4ba50d1ec3e5c11747605cfc5c8c2d4
Johannes Schrott <johannes.schrott@ppco.at>
2022-04-13 22:07:49 +0200

    Added F measure to automatically validate onset estimation correctnes
    Signed-off-by: Johannes Schrott <johannes.schrott@ppco.at>

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # 404-music-not-found
-Onset detection, tempo estimation and beat detection developed as part of the Special Topic "Audio and Music Processing" at JKU in summer semester 2022.
 
+Onset detection, tempo estimation and beat detection developed as part of the Special Topic "Audio and Music Processing"
+at JKU in summer semester 2022.
 
 The Rust project is missing the 404 in its name, as names starting with digits are not allowed :(
 
-
 Idea:
+
 * Rust commandline tool
 * Onset detection, tempo estimation and beat tracking possible and executed in parallel
 * Folders and single files can be processed
+
+All onset times are in seconds!
--- music_not_found/src/main.rs
+++ music_not_found/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::Path;
 
 use ansi_term::Style;
@@ -8,9 +9,17 @@ use rustfft::algorithm::Radix4;
 
 static ARG_FILE: &'static str = "--file";
 static ARG_HELP: &'static str = "--help";
+static ARG_VALIDATE: &'static str = "--validate";
 
 static N_ONSET: usize = 2048;
 static M_ONSET_SIGNAL_ENVELOPE: usize = 10;
+
+/// Accuracy in seconds of the estimated onsets
+static ONSET_ACCURACY: f64 = 50e-3;
+/// Accuracy in seconds of the estimated beats
+static BEAT_ACCURACY: f64 = 70e-3;
+/// Deviation of which the estimated tempo may be different (+ and -)
+static TEMPO_DEVIATION: f64 = 0.08;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -45,7 +54,7 @@ fn print_help() {
     println!("  {} <path>      Process the passed file", ARG_FILE);
     println!("  --folder <path>    Process all .wav files in the passed folder");
     println!("  --out <path>       Where to save the reuslt JSON file");
-    println!("  --train <path>     Compute the f-measure on the .gt files of the passed folder");
+//    println!("  {} <path>  Compute the f-measure on the .gt files of the passed folder", ARG_VALIDATE);
     println!("  --verbose          Display a lot of output status messages");
     println!("  {}             Displays this help message", ARG_HELP);
     println!("  ... more to be added");
@@ -73,7 +82,7 @@ fn process_file(file_path: &Path) {
         let mut planner = FftPlanner::new();
         let fft = planner.plan_fft_forward(N_ONSET);
         fft.process(&mut fft_buffer);
-        cur_pos += 1024;
+        cur_pos += (N_ONSET / 2); // TODO: evtl. nicht um /2 sonden um ganzen N_ONSET verschieben
     }
     let mut fft_buffer: Vec<Complex<f32>> = samples[cur_pos..samples.len()].iter().map(|&value| Complex::new(value, 0f32)).into_iter().collect::<Vec<_>>();
     let mut planner = FftPlanner::new();
@@ -82,5 +91,67 @@ fn process_file(file_path: &Path) {
 
     // everything run through the fourier transform, but the results are not used yet.
 
-    print!("Hi");
+    // Check if there is an *.onsets.gt file for the wav
+    let file_string_onsets_gt = [file_path.to_str().unwrap().strip_suffix(".wav").unwrap(), ".onsets.gt"].join("");
+
+    let mut found_onsets: Vec<f64> = Vec::new();
+
+
+    // onset detection should happen here (found onsets seconds have to be pushed to the vector)
+
+    found_onsets.push(0.2f64);
+
+
+    if Path::new(&file_string_onsets_gt).exists() { // if a onsets.gt file in the same folder exists, do a validation!
+        println!("Validation of Found onsets");
+        let gt_file = File::open(Path::new(&file_string_onsets_gt)).unwrap();
+        let reader = BufReader::new(gt_file);
+
+        /// Vector containing the true onset times (in seconds!)
+        let gt_onsets: Vec<f64> = reader.lines().map(|line| line.expect("Error on parsing line")).map(|line| line.parse::<f64>().unwrap()).collect();
+
+
+        f_measure_onsets(&found_onsets, &gt_onsets);
+    }
+}
+
+fn f_measure_onsets(found_onsets: &Vec<f64>, gt_onsets: &Vec<f64>) {
+    /// current index in vector of found onsets
+    let mut i_found: usize = 0;
+    /// current index in vector of gt onsets
+    let mut i_gt: usize = 0;
+
+    let mut t_p: usize = 0;
+    let t_n: usize = 0; // There are no true negatives!
+    let mut f_p: usize = 0;
+    let mut f_n: usize = 0;
+
+    if found_onsets.len() == 0 && gt_onsets.len() != 0 {
+        println!("No onsets found :( Something may have gone wrong");
+        return;
+    }
+    while i_found < found_onsets.len() && i_gt < gt_onsets.len() {
+        if gt_onsets[i_gt] - ONSET_ACCURACY <= found_onsets[i_found] && found_onsets[i_found] <= gt_onsets[i_gt] + ONSET_ACCURACY {
+            // the found onset is within the accuracy border
+            t_p += 1;
+            i_found += 1;
+            i_gt += 1;
+        } else if found_onsets[i_found] < gt_onsets[i_gt] - ONSET_ACCURACY {
+            f_p += 1;
+            i_found += 1;
+        } else if gt_onsets[i_gt] + ONSET_ACCURACY < found_onsets[i_found] {
+            f_n += 1;
+            i_gt += 1;
+        }
+    }
+    if i_gt < gt_onsets.len() {
+        f_n += gt_onsets.len() - i_gt;
+    }
+    let precision: f64 = t_p as f64 / (t_p as f64 + f_p as f64);
+    let recall: f64 = t_p as f64 / (t_p as f64 + f_n as f64);
+    let f_measure: f64 = 2f64 * (precision * recall) / (precision + recall);
+
+    println!("Precession: {}", precision);
+    println!("Recall:     {}", recall);
+    println!("F-Measure:  {}", f_measure);
 }
